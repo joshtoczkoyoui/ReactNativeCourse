2e1d314f4c8d88e1aa89e32cbf446914863dd2d0
Josh Toczko <josh.toczko@youi.tv>
2018-07-19 16:23:13 -0400

    AlbumListFile

--- Albums/index.js
+++ Albums/index.js
@@ -1,7 +1,13 @@
 // Import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { AppRegistry } from 'react-native';
+
+import Header from './src/components/Header';
 
 // Create a component
+const App = () => (
+    <Header headerText={'Albums'} />
+);
 
 // Render it to the device
+AppRegistry.registerComponent('Albums', () => App);
