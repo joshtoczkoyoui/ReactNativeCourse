56088bc443996218d19a8bc777b63dae4f29e7f5
Josh Toczko <josh.toczko@youi.tv>
2018-07-23 12:05:50 -0400

    Firebase!

--- Auth/src/App.js
+++ Auth/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import firebase from 'firebase';
+
+import { Header } from './components/common';
+
 export default class App extends React.Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyBih37tBncSr-By-et_ELBpZ9CVSXnPzCQ',
+            authDomain: 'auth-rncourse.firebaseapp.com',
+            databaseURL: 'https://auth-rncourse.firebaseio.com',
+            projectId: 'auth-rncourse',
+            storageBucket: 'auth-rncourse.appspot.com',
+            messagingSenderId: '78031554189'
+          });
+    }
+
     render() {
         return (
             <View>
-                <Text>!! Hi josh, app.js CLASS new location</Text>
+                <Header headerText="Authentication" />
+                <Text>App Latest</Text>
             </View>
         );
     }
