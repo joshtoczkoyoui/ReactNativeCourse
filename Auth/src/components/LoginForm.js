ad022f4b0ea44ec5e1363fcdedbee8a552041991
Josh Toczko <josh.toczko@youi.tv>
2018-07-23 14:09:47 -0400

    Cool passing of props to TextInput

--- Auth/src/components/LoginForm.js
+++ Auth/src/components/LoginForm.js
@@ -1,31 +1,40 @@
 import React from 'react';
-import { TextInput } from 'react-native';
+//import { TextInput } from 'react-native';
 
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 export default class LoginForm extends React.Component {
-    state = { text: '' };
+    state = { 
+        email: '',
+        password: '',
+    };
 
     render() {
         return (
             <Card>
                 <CardSection>
-                    <TextInput 
-                        value={this.state.text}
-                        onChangeText={text => this.setState({ text })}
-                        style={{ height: 40, width: 100 }}
+                    <Input
+                        label="Email"
+                        placeholder="username@gmail.com"
+                        value={this.state.email}
+                        onChangeText={email => this.setState({ email })}
+                        autoCorrect={false}
                     />
                 </CardSection>
 
                 <CardSection>
-                    <TextInput 
-                        value={this.state.text}
-                        onChangeText={text => this.setState({ text })}
+                    <Input
+                        label="Password"
+                        placeholder="password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                        autoCorrect={false}
+                        secureTextEntry
                     />
                 </CardSection>
                 
                 <CardSection>
-                    <Button onPress={() => { console.log(this.state.text); }}>
+                    <Button onPress={() => { console.log(this.state.email); }}>
                         Log in
                     </Button>
                 </CardSection>
