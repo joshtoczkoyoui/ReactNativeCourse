2d54d51c8f992058acdde3e0bedcd75fc3109ef3
Josh Toczko <josh.toczko@youi.tv>
2018-07-24 14:47:54 -0400

    ESLint and imported common stuff

---
+++ EmployeeManager/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Header } from './components/common';
+
+const App = () => (
+    <View>
+        <Header headerText='Manager' />
+        <Text>Hi josh new app emplyee</Text>
+    </View>
+);
+
+export default App;
