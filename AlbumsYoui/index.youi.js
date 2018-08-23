e6916a3de592ca8e04e38aeb05f138252129a968
Josh Toczko <josh.toczko@youi.tv>
2018-08-23 11:51:24 -0400

    Added in Albums source

--- AlbumsYoui/index.youi.js
+++ AlbumsYoui/index.youi.js
@@ -1,143 +1,17 @@
-/**
- * Sample app for testing text view to youi native view integrations.
- * @flow
- */
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
 
-import {
-  ButtonRef,
-  Composition,
-  ImageRef,
-  TextRef,
-  TimelineRef,
-  ViewRef
-} from '@youi/react-native-youi';
-import React, { Component } from 'react';
-import {
-  Animated,
-  AppRegistry,
-  Button,
-  FlatList,
-  Image,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
-import movies from './movies.js'
+// Create a component
+const YiReactApp = () => (
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
+);
 
-export default class YiReactApp extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      movieIndex: 0,
-      buttonEnabled: false
-    };
-  }
-
-  render() {
-    let movie = movies[this.state.movieIndex];
-    return (
-      <View
-        style={styles.container}
-      >
-        <Composition
-          source="PDP_Main"
-        >
-          <TimelineRef
-            name="In"
-            onLoad={(timeline) => {
-              this.inTimeline = timeline;
-            }}
-            onCompleted={() => {
-              this.setState({
-                buttonEnabled: true
-              });
-            }}
-          />
-          <TimelineRef
-            name="Out"
-            onLoad={(timeline) => {
-              this.outTimeline = timeline;
-            }}
-            onCompleted={() => {
-              this.setState({
-                movieIndex: this.nextMovieIndex
-              });
-            }}
-          />
-          <TextRef
-            name="Title-Text"
-            text={movie.title}
-          />
-          <TextRef
-            name="Details-Text"
-            text={movie.details}
-          />
-          <TextRef
-            name="Body-Text"
-            text={movie.synopsis}
-          />
-          <ViewRef name="controls" >
-            <ButtonRef
-              name="Btn-Previous"
-              text="Previous"
-              enabled={this.state.buttonEnabled}
-              onClick={() => {
-                this.nextMovieIndex = (this.state.movieIndex + movies.length - 1) % movies.length
-                this.outTimeline.play()
-                this.setState({
-                  buttonEnabled: false
-                })
-              }}
-            />
-            <ButtonRef
-              name="Btn-Next"
-              text="Next"
-              enabled={this.state.buttonEnabled}
-              onClick={() => {
-                this.nextMovieIndex = (this.state.movieIndex + 1) % movies.length
-                this.outTimeline.play()
-                this.setState({
-                  buttonEnabled: false
-                })
-              }}
-            />
-          </ViewRef>
-          <ImageRef
-            name="Image-2x3"
-            source={{"uri": "res://drawable/default/2x3-" + movie.image}}
-            onLoad={() => {
-                this.inTimeline.play()
-                console.log("OnLoad fired.")
-            }}
-            onLoadEnd={() => {
-                console.log("OnLoadEnd fired.")
-            }}
-            onLoadStart={() => {
-                console.log("OnLoadStart fired.")
-            }}
-            onError={() => {
-                console.log("OnError fired.")
-            }}
-          />
-          <ImageRef
-            name="Image-16x9"
-            source={{"uri": "res://drawable/default/16x9-" + movie.image}}
-          />
-        </Composition>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#d0d0d0'
-  },
-});
-
+// Render it to the device
 AppRegistry.registerComponent('YiReactApp', () => YiReactApp);
---
+++ AlbumsYoui/src/components/AlbumDetail.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { View, Text, Image, Linking } from 'react-native';
+
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+
+const AlbumDetail = ({ album }) => {
+    const { 
+        title,
+        artist,
+        url,
+        image,
+        thumbnail_image,
+    } = album;
+
+    const { 
+        headerContentStyle, 
+        thumbnailStyle, 
+        thumbnailContainerStyle,
+        headerTextStyle,
+        imageStyle,
+    } = styles;
+
+    return (
+        <Card>
+            <CardSection>
+                <View style={thumbnailContainerStyle}>
+                    <Image 
+                        style={thumbnailStyle}
+                        source={{ uri: thumbnail_image }} 
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+
+            <CardSection>
+                <Image 
+                    style={imageStyle}
+                    source={{ uri: image }} 
+                />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Album
+                </Button>
+            </CardSection>
+        </Card>
+    );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+    headerTextStyle: {
+        fontSize: 18,
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50,
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    imageStyle: {
+        height: 300,
+        width: null,
+        flex: 1,
+    },
+};
+
+export default AlbumDetail;
