e00dd5871eb43531ff6da46522dd4652820145a6
Josh Toczko <josh.toczko@youi.tv>
2018-08-23 11:49:58 -0400

    Not going to use Migrate

--- AlbumsYoui-Migrate/youi/SourceList.cmake
+++
@@ -1,5 +0,0 @@
-# =============================================================================
-# © You i Labs Inc. 2000-2018. All rights reserved.
-file(GLOB_RECURSE YI_PROJECT_SOURCE "src/*.cpp")
-
-file(GLOB_RECURSE YI_PROJECT_HEADERS "src/*.h")
