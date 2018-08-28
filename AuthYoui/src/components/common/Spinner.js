312f9c89b1ab0e33318aeaf78fc17a895e282b2e
Josh Toczko <josh.toczko@youi.tv>
2018-08-28 16:41:30 -0400

    No ActivityIndicator supported yet. Put in a placeholder

--- AuthYoui/src/components/common/Spinner.js
+++ AuthYoui/src/components/common/Spinner.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, ActivityIndicator } from 'react-native';
+import { View, Text } from 'react-native';
 
 const Spinner = ({ size }) => (
     <View style={styles.spinnerStyle}>
-        <ActivityIndicator size={size || 'large'} />
+        {/* <ActivityIndicator size={size || 'large'} /> */}
+        <Text>ActivityIndicatorPlaceholder</Text>
     </View>
 );
 
